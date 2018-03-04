8a5a0897f3a7f482e9c3abcfe03debaf5cd22651
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-03-04 12:09:41 +0530

    Create largeNumber.cpp

---
+++ largeNumber.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include<math.h>
+using namespace std;
+class largeNumber
+{
+    int n,i,j,ar[1000],s,d=0;
+    public:
+    largeNumber()
+    {
+    get();
+    sort();
+    display();
+    }
+    void get()
+    {
+    cout<<"INPUT"<<endl;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+    cin>>ar[i];
+    }
+    }
+    void sort()
+    {
+    for(i=0;i<n;i++)
+    {
+    for(j=i;j<n;j++)
+    {
+    if(ar[i]<ar[j])
+    {
+    int t=ar[i];
+    ar[i]=ar[j];
+    ar[j]=t;
+    }
+    }
+    }
+    for(i=0;i<n;i++)
+    { 
+    s=pow(10,n-1-i);
+    s*=ar[i];
+    d+=s;
+    }
+    }
+    void display()
+    {
+    cout<<"OUTPUT"<<endl;
+    cout<<endl;
+    cout<<d<<endl;
+    }
+};
+int main()
+{
+    largeNumber ln;
+return 0;
+}

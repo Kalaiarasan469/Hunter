0af1d52742d61b00a3c0850f4a857e624009a7ff
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 12:10:50 +0530

    Create Hunter8.cpp

---
+++ Hunter8.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Hunter8
+{
+private:
+int i,j,k,ar[1000],n;
+public:
+void get()
+{
+cin>>n;
+for(i=0;i<n;i++)
+{cin>>ar[i];}
+}
+void prnt()
+{
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+for(k=j+1;k<n;k++)
+{
+if(ar[i]+ar[j]==ar[k])
+{
+cout<<"\n"<<ar[i]<<" "<<ar[j]<<" "<<ar[k];
+}
+}
+}
+}
+}
+Hunter8()
+{
+get();
+prnt();
+}
+};
+int main() {
+Hunter8 h;
+return 0;
+}
